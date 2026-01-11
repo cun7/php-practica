c4fcf488a16e0e8d371f1819b50f6f5cd7c78773
Adilson Garcia <adicentro24@gmail.com>
2026-01-11 11:34:29 -0500

    Primer Proyecto PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$nombre = "Adicentro";
+$edad = 28;
+$activo = true;
+
+echo "Nombre: " . $nombre . "<br>";
+echo "Edad: " . $edad . "<br>";
+
+if ($edad >= 18) {
+    echo "Es mayor de edad";
+} else{
+    echo "Es menor de edad";
+}
